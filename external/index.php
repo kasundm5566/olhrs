91624fec68440493b04e2fe94e026d67392476ea
kasundm5566 <kasundinesh5566@gmail.com>
2016-08-11 16:28:45 +0530

    External system development started.

---
+++ external/index.php
@@ -0,0 +1,78 @@
+<?php
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome-Aqua Pearl Lake Resort</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
+        <link rel="stylesheet" href="css/scrollspy.css"/>
+        <link rel="stylesheet" href="css/site-layout.css"/>
+        <script src="js/jquery-1.12.2.min.js"></script>
+        <script src="bootstrap/js/bootstrap.min.js"></script>        
+    </head>
+    <body data-spy="scroll" data-target=".navbar" data-offset="50">
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Aqua Pearl Lake Resort</a>
+                </div>
+                <div>
+                    <div class="collapse navbar-collapse navbar-right" id="myNavbar">
+                        <ul class="nav navbar-nav">
+                            <li><a href="#welcome-section">Welcome</a></li>
+                            <li><a href="#overview-section">Overview</a></li>
+                            <li><a href="#gallery-section">Gallery</a></li>
+                            <li><a href="#reservation-section">Reservations</a></li>
+                            <li><a href="#contact-section">Contact us</a></li>
+                            <li><a href="#login-section">Login/Sign up</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+        <div id="welcome-section" class="container-fluid">
+            <h1>Welcome</h1>
+        </div>
+
+        <div id="overview-section" class="container-fluid">
+            <h1>Overview</h1>
+        </div>
+
+        <div id="gallery-section" class="container-fluid">
+            <h1>Gallery</h1>
+        </div>
+
+        <div id="reservation-section" class="container-fluid">
+            <h1>Reservations</h1>
+        </div>
+
+        <div id="contact-section" class="container-fluid">
+            <h1>Contact us</h1>
+        </div>
+
+        <div id="login-section" class="container-fluid">
+            <h1>Login</h1>
+        </div>       
+
+        <div id="site-footer">
+            Aqua Pearl Lake Resort-2016
+            <a target="_blank" title="find us on Facebook" href="http://www.facebook.com/PLACEHOLDER"><img alt="follow me on facebook" src="//login.create.net/images/icons/user/facebook_30x30.png" border=0></a>
+            <a target="_blank" title="follow me on twitter" href="http://www.twitter.com/PLACEHOLDER"><img alt="follow me on twitter" src="//login.create.net/images/icons/user/twitter-b_30x30.png" border=0></a>
+        </div>
+
+    </body>
+</html>
+
